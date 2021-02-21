b24d8f77eb7e8415864777e915741f5dd893f2cf
tomarv2 <varuntomar2019@gmail.com>
2021-02-21 11:00:58 -0800

    .

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "azurerm" {
+  # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
+  version           = "=1.28.0"
+//   subscription_id   = "8a641234-1234-1234-1234-122345678901"
+//   tenant_id         = "3dfaa4ad-1234-1234-1234-ef2a1db12345"
+    // subscription_id = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    // client_id       = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    // client_secret   = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    // tenant_id       = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+}
---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "key_name" {
+  description = "The SSH key name (NOTE: key should pre-exist)"
+}
+
+variable "Email" {
+  description = "email address to be used for tagging (suggestion: use group email address)"
+}
+
+variable "teamid" {
+  description = "(Required) Name of the team/group e.g. devops, dataengineering. Should not be changed after running 'tf apply'"
+}
+
+variable "prjid" {
+  description = "(Required) Name of the project/stack e.g: mystack, nifieks, demoaci. Should not be changed after running 'tf apply'"
+}
+
+variable "rg_name" {
+  description = "Resource Group name"
+}
+
+variable "nsg_name" {
+}
+
+
+variable "vnet_name" {
+}
+
+variable "subnet_name" {
+}
+
+variable "rg_location" {
+}
+
+variable "address_block" {
+}
+
+variable "subscription_id" {
+}
+
