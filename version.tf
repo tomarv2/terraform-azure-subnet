8e3f78c21c6f170523cd31ab3975ee4bb6d4b4fa
tomarv2 <varuntomar2019@gmail.com>
2021-02-24 07:44:59 -0800

    moving version to seperate file

---
+++ version.tf
@@ -0,0 +1,19 @@
+  # Note: versioning is optional but strongly recommended to pin the version of the Provider
+  terraform {
+  required_version            = ">= 0.14"
+  required_providers {
+    azurerm = {
+      version                     = "~> 2.48"
+    }
+  }
+}
+
+# NOTE: Noticed an issue with getting access to Azure even after setting env.
+# variables, adding it to mitigate auth issues.
+provider "azurerm" {
+  features {}
+  subscription_id             = var.subscription_id
+  client_id                   = var.client_id
+  client_secret               = var.client_secret
+  tenant_id                   = var.tenant_id
+}
